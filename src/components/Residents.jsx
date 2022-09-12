fdc2ed5b35646aa110fcc8e806ad65ae473eec88
Davidzambrano16 <david-z-p@hotmail.com>
2022-09-12 03:31:46 -0500

    commit

--- src/components/Residents.jsx
+++ src/components/Residents.jsx
@@ -5,20 +5,36 @@ import React, { useEffect, useState } from 'react';
 const Residents = ({ character }) => {
 
     const [residents, setResidents] = useState({})
+    let color = "gray"
 
-    useEffect( () => {
+    useEffect(() => {
         axios.get(character)
             .then(res => setResidents(res.data))
-    }, [] )
+    }, [])
+
+
+    residents.status === "Alive" ? (
+        color = "green"
+    ) : (
+        residents.status === "Dead" ? (
+            color = "red"
+        ) : (
+            color = "gray"
+        )
+    )
 
     return (
         <div className='card'>
+            <div className='status'>
+                <div className='icon--status' style={{ backgroundColor: color }}></div>
+                <h3 className='text--status'> {residents.status} </h3>
+            </div>
             <img className='img--card' src={residents.image} alt="" />
             <div className='text--card'>
                 <h3> {residents.name} </h3>
                 <hr />
                 <br />
-                <h4>SPECIES</h4>
+                <h4>RACE</h4>
                 <h3> {residents.species} </h3>
                 <br />
                 <h4>ORIGIN</h4>
